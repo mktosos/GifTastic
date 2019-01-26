14136032d404f5096f3555a0d12a99fe5b05a417
mktosos <taniguchidesigns@hotmail.com>
2019-01-25 16:27:51 -0800

    buttons pulling gifs addded

--- GifTastic.js
+++ GifTastic.js
@@ -1,5 +1,41 @@
-var topics = [];
-for (let index = 0; index < array.length; index++) {
-    $("<button>");
-    
+var topics = ["gameboy", "gameboy color", "gameboy advanced", "gba"]
+for (let index = 0; index < topics.length; index++) {
+    var dpIndex = "data-person"+ index
+    var button = $("#buttonDiv").append("<button class=" + dpIndex +">");
+    var topicsIndex = (topics[index]);
+    $('button.'+dpIndex).attr("data-person",topicsIndex);
+    console.log(dpIndex);
 }
+//$("button").on("click",grabGifs());
+ 
+$("button").on("click", function() {
+    console.log("hi")
+    var person = $(this).attr("data-person");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      person + "&api_key=dc6zaTOxFJmzC&limit=10";
+      
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div>");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var personImage = $("<img>");
+          personImage.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.prepend(p);
+          gifDiv.prepend(personImage);
+
+          $("#gifs-appear-here").prepend(gifDiv);
+          console.log("hi")
+        }
+      });
+  });
